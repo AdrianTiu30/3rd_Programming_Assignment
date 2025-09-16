68c995fc3c4af3fdde32c793b0cd1e25ea20f3ad
AdrianTiu30 <marcadrianytiu@gmail.com>
2025-09-16 21:49:20 +0800

    Add files via upload

---
+++ TIU_Pandas-P1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+cars = pd.read_csv('cars.csv')
+cars
+
+
+# In[3]:
+
+
+cars.head()
+
+
+# In[4]:
+
+
+cars.tail()
+
+
+# In[ ]:
+
+
+
+
